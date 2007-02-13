a29072cc4fb5ec6b57e5bcfd209ed4cb4a7c6e67
Oussama Kanoun <okanoun@chico.laas.fr>
2007-02-13 16:18:43 +0100

    modifications mainly on jrlRobotMotion.h

--- interfaces/jrlRobotMotion.h
+++ interfaces/jrlRobotMotion.h
@@ -1,7 +1,7 @@
 #ifndef JRL_ROBOT_MOTION_H
 #define JRL_ROBOT_MOTION_H
 
-#include "jrlDynamicRobot.h"
+#include "jrlHumanoidDynamicRobot.h"
 
 /**
    \brief Defines the motion of a robot along time.
@@ -32,34 +32,23 @@ public:
     /**
     \brief Get robot for which motion is defined.
      */
-    virtual const CjrlDynamicRobot<Mnxp,M4x4,M3x3,Vn,V3>& robot() = 0;
+    virtual const CjrlHumanoidDynamicRobot<Mnxp,M4x4,M3x3,Vn,V3>& robot() = 0;
 
     /**
     \brief Get Configuration at given time.
-
-    \return The configuration vector.
      */
-    virtual Vn configAtTime(double inTime) = 0;
+    virtual bool configAtTime(double inTime, Vn& outConfig) = 0;
 
     /**
     \brief Get velocity at given time.
      */
-    virtual V3 velocityAtTime(double inTime) = 0;
+    virtual bool velocityAtTime(double inTime, V3& outVector3) = 0;
 
     /**
     \brief Get Acceleration at given time.
      */
-    virtual V3 accelerationAtTime(double inTime) = 0;
-    
-    /**
-    \brief Append a new configuration at the end of the current motion (must be in the implementation )
-     */
-    virtual void appendConfig(const Vn&) = 0;
-    
-    /**
-    \brief Clear stored motion
-     */
-    virtual void clear() = 0;
+    virtual bool accelerationAtTime(double inTime, V3& outVector3) = 0;
+
 };
 
 #endif
