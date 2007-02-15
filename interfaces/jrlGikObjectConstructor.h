8b634bfb7974ce38f78175ced84026b65691816a
Oussama Kanoun <okanoun@jolimont.laas.fr>
2007-02-15 17:36:06 +0100

    corrected the qualifiers to make the class abstract

--- interfaces/jrlGikObjectConstructor.h
+++ interfaces/jrlGikObjectConstructor.h
@@ -19,7 +19,7 @@ public:
     \param inRobot : associated robot.
     \param inRobot : motion sampling period.
      */
-    static CjrlGikWholeBodyMotionPlanner* createWholeBodyMotionPlanner(CjrlHumanoidDynamicRobot& inRobot, double inSamplingPeriod);
+    virtual static CjrlGikWholeBodyMotionPlanner* createWholeBodyMotionPlanner(CjrlHumanoidDynamicRobot& inRobot, double inSamplingPeriod) = 0;
 
     /**
     \brief Construct and return a pointer to a 3D Position state constraint on a point connected to a given joint of the robot.
@@ -28,7 +28,7 @@ public:
     \param inLocalPoint : point given in inJoint's frame and to be moved to inWorldTarget.
     \param inTargetWorldPoint : target point in the work space.
      */
-    static CjrlGik3DPosConstraint* create3DPositionConstraint(CjrlHumanoidDynamicRobot& inRobot, CjrlJoint& inJoint, const vector3& inLocalPoint,const vector3& inTargetWorldPoint);
+    virtual static CjrlGik3DPosConstraint* create3DPositionConstraint(CjrlHumanoidDynamicRobot& inRobot, CjrlJoint& inJoint, const vector3& inLocalPoint,const vector3& inTargetWorldPoint) =0;
         
     /**
     \brief Construct and return a pointer to a 6D full state constraint (3D position and orientation matrix) on a point connected to a given joint of the robot.
@@ -38,7 +38,7 @@ public:
     \param inTargetWorldPoint : target point in the work space.
     \param inTargetOrientation : target orientation matrix in world frame
      */
-    static CjrlGik6DFullConstraint* create6DFullConstraint(CjrlHumanoidDynamicRobot& inRobot, CjrlJoint& inJoint, const vector3& inLocalPoint,const vector3& inTargetWorldPoint,const matrix3& inTargetOrientation);
+    virtual static CjrlGik6DFullConstraint* create6DFullConstraint(CjrlHumanoidDynamicRobot& inRobot, CjrlJoint& inJoint, const vector3& inLocalPoint,const vector3& inTargetWorldPoint,const matrix3& inTargetOrientation) =0;
     
     /**
     \brief Construct and return a pointer to a 6D full state constraint (transformation) on a point connected to a given joint of the robot.
@@ -47,7 +47,7 @@ public:
     \param inLocalPoint : point given in inJoint's frame and to be moved to inWorldTarget.
     \param inTargetWorldTransformation : target point in the work space.
      */
-    static CjrlGik6DFullConstraint* create6DFullConstraint(CjrlHumanoidDynamicRobot& inRobot, CjrlJoint& inJoint, const vector3& inLocalPoint,const matrix4& inTargetWorldTransformation);
+    virtual static CjrlGik6DFullConstraint* create6DFullConstraint(CjrlHumanoidDynamicRobot& inRobot, CjrlJoint& inJoint, const vector3& inLocalPoint,const matrix4& inTargetWorldTransformation) =0;
 
     /**
     \brief Construct and return a pointer to a 3D Orientation state constraint on a joint of the robot.
@@ -55,7 +55,7 @@ public:
     \param inJoint : associated joint.
     \param inTargetOrientation : target orientation matrix in world frame
      */
-    static CjrlGik3DOrientConstraint* create3DOrientationConstraint(CjrlHumanoidDynamicRobot& inRobot, CjrlJoint& inJoint, const matrix3& inTargetOrientation);
+    virtual static CjrlGik3DOrientConstraint* create3DOrientationConstraint(CjrlHumanoidDynamicRobot& inRobot, CjrlJoint& inJoint, const matrix3& inTargetOrientation) =0;
     
     /**
     \brief Construct and return a pointer to a 2D Orientation state constraint on a joint of the robot.
@@ -64,7 +64,7 @@ public:
     \param inLocalVector : vector given in inJoint's frame and to be aligned with inTargetWorldVector.
     \param inTargetWorldVector : target point in the work space.
      */
-    static CjrlGik2DOrientConstraint* create2DOrientationConstraint(CjrlHumanoidDynamicRobot& inRobot, CjrlJoint& inJoint,, const vector3& inLocalVector,const vector3& inTargetWorldVector);
+    virtual static CjrlGik2DOrientConstraint* create2DOrientationConstraint(CjrlHumanoidDynamicRobot& inRobot, CjrlJoint& inJoint, const vector3& inLocalVector,const vector3& inTargetWorldVector) =0;
 
     /**
     \brief Construct and return a pointer to a 1D Position state constraint on a point connected to a given joint of the robot.
@@ -74,13 +74,13 @@ public:
     \param inTargetPlanePoint : a point of the target plane in world frame (see CjrlGik1DPosConstraint class definition for details).
     \param inTargetPlaneNormal : a normal vector of the plane in world frame (see CjrlGik1DPosConstraint class definition for details).
      */
-    static CjrlGik1DPosConstraint* create1DPositionConstraint(CjrlHumanoidDynamicRobot& inRobot, CjrlJoint& inJoint, const vector3& inLocalPoint,const vector3& inTargetPlanePoint, const vector3& inTargetPlaneNormal);
+    virtual static CjrlGik1DPosConstraint* create1DPositionConstraint(CjrlHumanoidDynamicRobot& inRobot, CjrlJoint& inJoint, const vector3& inLocalPoint,const vector3& inTargetPlanePoint, const vector3& inTargetPlaneNormal) =0;
     
     /**
     \brief Construct and return a pointer to a Motion constraint.
     \param inSamplingPeriod : the interval of time between two successsive state constraints.
      */
-    static CjrlGikMotionConstraint* createMotionConstraint(double inSamplingPeriod);
+    virtual static CjrlGikMotionConstraint* createMotionConstraint(double inSamplingPeriod) =0;
 };
 
 #endif
