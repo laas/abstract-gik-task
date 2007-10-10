7b29bb400daf320199e1582c0fee1f37123505ec
florent <florent@localhost.localdomain>
2007-10-10 21:25:39 +0200

    Correction in documentation.

--- documentation/additionalDoc/gikTask.h
+++ documentation/additionalDoc/gikTask.h
@@ -1,20 +1,20 @@
 /** \mainpage 
-\section sec_intro Introduction
+\section abstGikTask_sec_intro Introduction
 
 We propose in the following header files abstract interfaces to define
 whole body tasks planning for a humanoid robot.  The goal is to provide a standard within JRL
 developments in order to make packages dealing with humanoid robots
 compatible with each other.
 
-\section sec_howto How it works
+\section abstGikTask_sec_howto How it works
 
-\subsection subsec_user For the users
+\subsection abstGikTask_subsec_user For the users
 
 Packages using whole body task planning are guaranted to be compatible to any
 package implementing the abstract interfaces, as long as they use the
 classes defined here.
 
-\subsection subsec_developpers For the developers
+\subsection abstGikTask_subsec_developpers For the developers
 
 A developper implementing classes deriving from the abstract interface classes can
 share his work with the user of the above section.
