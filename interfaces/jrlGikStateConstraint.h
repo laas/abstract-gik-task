5da4326c60cca3f854e858ad5135370920e769a8
Oussama Kanoun <okanoun@cleo.laas.fr>
2009-02-23 11:40:43 +0100

    New class for linear constraint specification, serving as base for CjrlGikStateConstraint.

--- interfaces/jrlGikStateConstraint.h
+++ interfaces/jrlGikStateConstraint.h
@@ -9,40 +9,41 @@
 #define JRL_STATE_CONSTRAINT_H
 
 #include "robotDynamics/jrlHumanoidDynamicRobot.h"
+#include "jrlLinearConstraint.h"
 
 /**
-\brief Specify a Constraint over the state of a humanoid robot.
+\brief Specify a linear constraint over the state of a humanoid robot.
 */
 
 
-class CjrlGikStateConstraint
+class CjrlGikStateConstraint: virtual public CjrlLinearConstraint
 {
 public:
-    /**
-    \brief Destructor
-     */
-    virtual ~CjrlGikStateConstraint()
-    {}
 
     /**
-    \brief Copy
+    \brief Copy constructor.
      */
     virtual CjrlGikStateConstraint* clone() const =0;
 
     /**
-    \name Definition of the constraint
+    \name Definition of the constraint.
     @{
     */
 
     /**
-    \brief Get associated robot
+    \brief Get associated robot.
      */
     virtual CjrlDynamicRobot& robot() = 0;
 
     /**
-    \brief Get the dimension of the constraint.
-    */
-    virtual unsigned int dimension() const = 0;
+    \brief Select the joint in the robot that serves as root for computation of jacobians.
+     */
+    virtual void jacobianRoot(CjrlJoint& inJoint) =0 ;
+
+    /**
+    \brief Get the influencing dofs vector which is a binary vector whose size matches the robot cnfiguration's, where an element with value 1 indicates that the corresponding degree of freedom can modify the value of this constraint and an element with value 0 cannot.
+     */
+    virtual vectorN& influencingDofs() = 0;
 
     /**
     @}
@@ -54,14 +55,9 @@ public:
      */
 
     /**
-    \brief Compute a binary vector whose size matches the robot cnfiguration's, where an element with value 1 indicates that the corresponding degree of freedom can modify the value of this constraint, and an element with value 0 cannot.
+    \brief Compute the influencing dofs vector.
      */
     virtual void computeInfluencingDofs() = 0;
-
-    /**
-    \brief Get the influencing dofs
-     */
-    virtual vectorN& influencingDofs() = 0;
 
     /**
     \brief Compute the value of the constraint.
@@ -69,33 +65,9 @@ public:
     virtual void computeValue() = 0;
 
     /**
-    \brief Compute the Jacobian of the constraint with respect to the internal degrees of freedom and to a selected root joint (see method CjrlGikStateConstraint::jacobianRoot())
+    \brief Compute the Jacobian of the constraint with respect to the internal degrees of freedom and to a selected root joint (see method CjrlGikStateConstraint::jacobianRoot()).
      */
     virtual void computeJacobian() = 0;
-
-    /**
-    \brief Select the joint in the robot that serves as root for computation of jacobians.
-    */
-    virtual void jacobianRoot(CjrlJoint& inJoint) =0 ;
-
-    /**
-    @}
-     */
-
-    /**
-    \name Getting result of computations
-    @{
-     */
-
-    /**
-    \brief Get the constraint value.
-     */
-    virtual const vectorN& value() = 0;
-
-    /**
-    \brief Get the constraint Jacobian
-     */
-    virtual const matrixNxP& jacobian() = 0;
 
     /**
     @}
